ab60e327455bdedb3a3540e6a20f553e37abbbd0
luc-tuyishime <luctunechi45@gmail.com>
2021-10-19 15:50:21 +0200

    first commit

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (_, res) => res.send("Welcome to Madiba System..."));
+router.get("/", (_, res) => res.send("Welcome to Madiba System"));
 
 export default router;
