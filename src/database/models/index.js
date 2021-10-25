c0f1ea8e14830fc85ffefae0b77a7caa09088a46
luc-tuyishime <luctunechi45@gmail.com>
2021-10-25 21:12:06 +0200

    add seed folder

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,7 +1,7 @@
-import "dotenv/config";
-import fs from "fs";
-import path from "path";
-import { Sequelize } from "sequelize";
+// import "dotenv/config";
+const fs = require("fs");
+const path = require("path");
+const { Sequelize } = require("sequelize");
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -17,18 +17,19 @@ const { database, username, password, ...options } = config;
 const sequelize = new Sequelize(database, username, password, options);
 
 fs.readdirSync(__dirname)
-    .filter(
-        (file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    )
-    .forEach((file) => {
-        const model = require(path.join(__dirname, file))(sequelize);
-        db[model.name] = model;
-    });
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize);
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach((modelName) => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
